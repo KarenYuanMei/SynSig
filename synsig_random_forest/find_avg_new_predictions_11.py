86bc13a36b36bac92cec1cf83e7ec575fdbd162e
Karen mei <karenmei@Karens-MacBook-Pro.local>
2020-08-28 14:22:31 -0700

    put all code into modularized functions

--- synsig_random_forest/find_avg_new_predictions_11.py
+++ synsig_random_forest/find_avg_new_predictions_11.py
@@ -33,38 +33,43 @@ def find_data_genes(training_genes):
 	return data_genes
 
 
-pos_file='synapse_positives.csv'
-neg_file='synapse_negatives.csv'
+def find_avg_scores():
+	pos_file='synapse_positives.csv'
+	neg_file='synapse_negatives.csv'
 
-#make object pairs of pos_training and new gene objects:
-training=get_all_training(pos_file, neg_file)
-data_genes=find_data_genes(training)
+	#make object pairs of pos_training and new gene objects:
+	training=get_all_training(pos_file, neg_file)
+	data_genes=find_data_genes(training)
 
 
-print ('data_genes', len(data_genes))
+	print ('data_genes', len(data_genes))
 
-pred_filename='all_gene_predictions.csv'
-pred=pd.read_csv(pred_filename, index_col=[0])
-gene1=pred['Gene1'].tolist()
-gene2=pred['Gene2'].tolist()
+	pred_filename='all_gene_predictions.csv'
+	pred=pd.read_csv(pred_filename, index_col=[0])
+	gene1=pred['Gene1'].tolist()
+	gene2=pred['Gene2'].tolist()
 
-avg_scores=[]
-novel_genes=[]
-for gene in data_genes:
-	df1=pred.loc[pred['Gene1'] == gene]
-	
-	df2=pred.loc[pred['Gene2'] == gene]
-	df=df1.append(df2)
-	scores=df['ypredict'].tolist()
-	print ('length of scores', len(scores))
-	scores_np=np.array(scores)
-	avg_score=np.mean(scores_np)
-	
-	avg_scores.append(avg_score)
-	novel_genes.append(gene)
+	avg_scores=[]
+	novel_genes=[]
+	for gene in data_genes:
+		df1=pred.loc[pred['Gene1'] == gene]
+		
+		df2=pred.loc[pred['Gene2'] == gene]
+		df=df1.append(df2)
+		scores=df['ypredict'].tolist()
+		print ('length of scores', len(scores))
+		scores_np=np.array(scores)
+		avg_score=np.mean(scores_np)
+		
+		avg_scores.append(avg_score)
+		novel_genes.append(gene)
 
-print ('novel_genes', len(novel_genes), 'all_average_scores', len(avg_scores))
+	print ('novel_genes', len(novel_genes), 'all_average_scores', len(avg_scores))
 
-df=pd.DataFrame({'genes': novel_genes, 'avg_scores': avg_scores})
-df.to_csv('brain_RNA_big_pool_novel_synapse_genes_avg_scores.csv')
+	df=pd.DataFrame({'genes': novel_genes, 'avg_scores': avg_scores})
+	df.to_csv('brain_RNA_big_pool_novel_synapse_genes_avg_scores.csv')
+	return df
+
+if __name__ == '__main__':
+	find_avg_scores()
 
